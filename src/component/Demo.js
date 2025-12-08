2252b139eed0e7a7d7b319a13d55daea1550488f
thruy <huuhoang.3002@gmail.com>
2025-12-08 12:32:41 +0700

    conditional ouput in react

--- src/component/Demo.js
+++ src/component/Demo.js
@@ -3,15 +3,19 @@ import UserInfo from "./UserInfo";
 import DisplayInfor from "./DisplayInfor";
 
 class Demo extends React.Component {
+    users = [
+        { id: 1, name: 'Hoang', age: 25 },
+        { id: 2, name: 'Thruy', age: 12 },
+        { id: 3, name: 'Thao', age: 23 }
+    ]
     //jsx
     render() {
         return (
             <div>
                 <UserInfo />
                 <hr />
-                <DisplayInfor name="Thruy" age="20" />
+                <DisplayInfor listUsers={this.users} />
                 <br />
-                <DisplayInfor name="Thao" age={22} />
             </div>
         )
     }
--- src/component/DisplayInfor.js
+++ src/component/DisplayInfor.js
@@ -1,15 +1,39 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
-
+    state = {
+        isShowListUser: true
+    }
+    handleHideUser() {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
     render() {
         //console.log(this.props);
-        const { name, age } = this.props;
+        const { listUsers } = this.props;
+        console.log('Check list user: ', listUsers);
+        console.table(listUsers);
         return (
             <div>
-                <div>Display this information: </div>
-                <p>My name is {name}</p>
-                <p>My age: {age} years old.</p>
+                <div>
+                    <span onClick={(e) => this.handleHideUser(e)}>{this.state.isShowListUser ? "Hide list users" : "Show list user"}</span>
+                </div>
+                <div>Display users information: </div>
+                {this.state.isShowListUser &&
+                    <div>
+                        {listUsers.map((user) => {
+                            return (
+                                <div key={user.id} className={user.age >= 18 ? "blue" : "red"}>
+                                    <p>My name is {user.name}, My age: {user.age} years old.</p>
+                                    <hr />
+                                </div>
+                            )
+                        })}
+                        {/* <p>My name is {name}</p>
+                <p>My age: {age} years old.</p> */}
+                    </div>
+                }
             </div>
         )
     }
