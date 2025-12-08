baeee60c67edb59beec7e899d970b9111780a99e
thruy <huuhoang.3002@gmail.com>
2025-12-08 08:56:16 +0700

    demo event react

--- src/component/Demo.js
+++ src/component/Demo.js
@@ -6,12 +6,24 @@ class Demo extends React.Component {
         Address: 'Vinh',
         age: 25
     }
+
+    handleClick(event) {
+        console.log("Data changed");
+        console.log(event);
+    }
+
+    handleOnMouseOver(event) {
+        console.log("Mouse over event");
+        console.log(event.pageX, event.pageY);
+    }
     //jsx
     render() {
         return (
             <div>My first component
                 <p>Demo component with statistic {Math.random()}</p>
                 <div>My name: {this.state.name}, {this.state.age} years old and from {this.state.Address}</div>
+                <button onMouseOver={this.handleOnMouseOver}>onMouseOver</button>
+                <button onClick={this.handleClick}>onClick</button>
             </div>
 
         )
