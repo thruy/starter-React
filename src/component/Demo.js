f322af6b4cb8727aa93ce8e88b5722a98d1ffb82
thruy <huuhoang.3002@gmail.com>
2025-12-08 08:15:06 +0700

    demo first component

---
+++ src/component/Demo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Demo extends React.Component {
+    //jsx
+    render() {
+        return (
+            <div>My first component
+                <p>Demo component</p>
+            </div>
+
+        )
+    }
+}
+
+export default Demo;
