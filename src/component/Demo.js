daab854e942e508eb787bcc50cca5fd3a952e221
thruy <huuhoang.3002@gmail.com>
2025-12-08 10:05:24 +0700

    form in react

--- src/component/Demo.js
+++ src/component/Demo.js
@@ -21,14 +21,32 @@ class Demo extends React.Component {
         console.log("Mouse over event");
         console.log(event.pageX, event.pageY);
     }
+
+    handleOnChangInput(event) {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnSubmit(event) {
+        event.preventDefault();
+        this.setState({
+            name: event.target[0].value
+        })
+        console.log(this.state);
+    }
     //jsx
     render() {
         return (
             <div>My first component
                 <p>Demo component with statistic {Math.random()}</p>
-                <div>My name: {this.state.name}, {this.state.age} years old and from {this.state.Address}</div>
-                <button onMouseOver={this.handleOnMouseOver}>onMouseOver</button>
-                <button onClick={(e) => { this.handleClick(e) }}>onClick</button>
+                <div>My name is: {this.state.name}, {this.state.age} years old and from {this.state.Address}</div>
+                {/* <button onMouseOver={this.handleOnMouseOver}>onMouseOver</button>
+                <button onClick={(e) => { this.handleClick(e) }}>onClick</button> */}
+                <form onSubmit={(e) => this.handleOnSubmit(e)}>
+                    <input type="text"></input>
+                    <button >Submit</button>
+                </form>
             </div>
 
         )
