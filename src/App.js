a659569efceddd212b12074de62c47b6b390970d
thruy <huuhoang.3002@gmail.com>
2025-12-08 08:27:38 +0700

    first state demo

--- src/App.js
+++ src/App.js
@@ -3,27 +3,49 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import Demo from './component/Demo';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import React from 'react';
 
-  return (
-    <div>
-      <div>My demo component</div>
-      <Demo></Demo>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hello World, Hoang.
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>My demo component</div>
+        <Demo></Demo>
+      </div>
+    )
+  }
 }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div>My demo component</div>
+//       <Demo></Demo>
+//     </div>
+//   );
+// }
+
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
+
+//   return (
+//     <div>
+//       <div>My demo component</div>
+//       <Demo></Demo>
+//     </div>
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello World, Hoang.
+//         </p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/component/Demo.js
+++ src/component/Demo.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 class Demo extends React.Component {
+    state = {
+        name: 'Hoang',
+        Address: 'Vinh',
+        age: 25
+    }
     //jsx
     render() {
         return (
             <div>My first component
-                <p>Demo component</p>
+                <p>Demo component with statistic {Math.random()}</p>
+                <div>My name: {this.state.name}, {this.state.age} years old and from {this.state.Address}</div>
             </div>
 
         )
