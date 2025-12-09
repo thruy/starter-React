361564fe370887b5e4e00ad8ed95b26194e19f0b
thruy <huuhoang.3002@gmail.com>
2025-12-09 22:03:45 +0700

    image in react

--- src/component/DisplayInfor.js
+++ src/component/DisplayInfor.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './DisplayInfor.scss';
+import image from '../logo.svg';
+// khong nen luu file anh trong public vi se khong duoc xu ly boi webpack
 
 class DisplayInfor extends React.Component {
     state = {
@@ -17,6 +19,7 @@ class DisplayInfor extends React.Component {
         //console.table(listUsers);
         return (
             <div className="display-info-container">
+                <img src={image} alt="react-image" />
                 <div>
                     <span onClick={(e) => this.handleHideUser(e)}>{this.state.isShowListUser ? "Hide list users" : "Show list user"}</span>
                 </div>
